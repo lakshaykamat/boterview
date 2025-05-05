6350b579816bd3aba6b43c8050bcbefb33aaae59
Lakshay Kamat <lakshaykamat2048@gmail.com>
2025-05-05 12:51:31 +0530

    Enhance email scheduling by specifying IST timezone and improving logging

--- jobs/scheduler.js
+++ jobs/scheduler.js
@@ -2,11 +2,15 @@ const cron = require('node-cron');
 const sendNextQuestion = require('../controllers/emailController');
 
 const scheduleEmails = () => {
+  // Define the times in IST (India Standard Time)
   const times = ['0 9 * * *', '0 12 * * *', '0 15 * * *', '0 18 * * *', '0 21 * * *'];
 
+  // Schedule the jobs using IST timezone
   times.forEach((time) => {
-    cron.schedule(time, sendNextQuestion);
-    console.log(`📆 Scheduled question email at: ${time}`);
+    cron.schedule(time, sendNextQuestion, {
+      timezone: 'Asia/Kolkata'  // Specify the timezone as IST
+    });
+    console.log(`📆 Scheduled question email at: ${time} IST`);
   });
 };
 
