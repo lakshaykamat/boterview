21e410554f98cbb0ce6c0815583d5b9d50b92939
Lakshay Kamat <lakshaykamat2048@gmail.com>
2025-06-21 22:07:48 +0530

    refactor: update time condition for retrying failed messages from 5 minutes to 1 hour

--- controllers/retryFailedMessages.js
+++ controllers/retryFailedMessages.js
@@ -3,13 +3,14 @@ const { bot } = require("../bot/telegramBot");
 
 const retryFailedMessages = async () => {
   const oneDayAgo = new Date(Date.now() - 24 * 60 * 60 * 1000);   // 24 hours ago
-  const fiveMinutesAgo = new Date(Date.now() - 5 * 60 * 1000);    // 5 minutes ago
+  const oneHourAgo = new Date(Date.now() - 60 * 60 * 1000);       // 1 hour ago
+
 
   const failedLogs = await MessageLog.find({
     success: false,
     retryCount: { $lt: 10 },
     sentAt: { $gte: oneDayAgo },
-    lastTriedAt: { $lt: fiveMinutesAgo }
+    lastTriedAt: { $lt: oneHourAgo }
   });
 
   for (const log of failedLogs) {
@@ -26,7 +27,7 @@ const retryFailedMessages = async () => {
     await log.save();
   }
 
-  console.log(`🔁 Retried ${failedLogs.length} failed messages`);
+  console.log(`Retried ${failedLogs.length} failed messages`);
 };
 
 module.exports = retryFailedMessages;
